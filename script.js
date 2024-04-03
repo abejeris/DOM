d4078b1f94cb0531526f3e31ba0e1b506340f9a2
Audrius Bejeris <audriusbejeris@gmail.com>
2024-04-03 15:47:33 +0300

    silly clock added

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ const inputs = document.getElementsByTagName('input');
 const specialLi = document.getElementsByClassName('special');
 
 const h1 = document.querySelector('h1');
+
+const range = document.querySelector('#range');
+const inner = document.querySelector('#inner');
+
+range.addEventListener('input', function () {
+	inner.style.transform = `rotate(${range.value}deg)`;
+});
