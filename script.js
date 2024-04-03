5fc3c778fd0a181178081780167b17991a1e92a5
Audrius Bejeris <audriusbejeris@gmail.com>
2024-04-03 13:12:52 +0300

    DOM manipulation

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('Hello World');
+document.getElementById('ai-photo').style.width = '700px';
+
+const inputs = document.getElementsByTagName('input');
+
+const specialLi = document.getElementsByClassName('special');
+
+const h1 = document.querySelector('h1');
