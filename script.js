ed5a5d350a5883abf09ff6c08b7b9249f56efbdc
Audrius Bejeris <audriusbejeris@gmail.com>
2024-04-04 13:44:23 +0300

    NBA game messy javascript added

--- script.js
+++ script.js
@@ -1,14 +1,116 @@
-document.getElementById('ai-photo').style.width = '700px';
+const warriorsGames = [
+	{
+		awayTeam: {
+			team: 'Golden State',
+			points: 119,
+			isWinner: true,
+		},
+		homeTeam: {
+			team: 'Houston',
+			points: 106,
+			isWinner: false,
+		},
+	},
+	{
+		awayTeam: {
+			team: 'Golden State',
+			points: 105,
+			isWinner: false,
+		},
+		homeTeam: {
+			team: 'Houston',
+			points: 127,
+			isWinner: true,
+		},
+	},
+	{
+		homeTeam: {
+			team: 'Golden State',
+			points: 126,
+			isWinner: true,
+		},
+		awayTeam: {
+			team: 'Houston',
+			points: 85,
+			isWinner: false,
+		},
+	},
+	{
+		homeTeam: {
+			team: 'Golden State',
+			points: 92,
+			isWinner: false,
+		},
+		awayTeam: {
+			team: 'Houston',
+			points: 95,
+			isWinner: true,
+		},
+	},
+	{
+		awayTeam: {
+			team: 'Golden State',
+			points: 94,
+			isWinner: false,
+		},
+		homeTeam: {
+			team: 'Houston',
+			points: 98,
+			isWinner: true,
+		},
+	},
+	{
+		homeTeam: {
+			team: 'Golden State',
+			points: 115,
+			isWinner: true,
+		},
+		awayTeam: {
+			team: 'Houston',
+			points: 86,
+			isWinner: false,
+		},
+	},
+	{
+		awayTeam: {
+			team: 'Golden State',
+			points: 101,
+			isWinner: true,
+		},
+		homeTeam: {
+			team: 'Houston',
+			points: 92,
+			isWinner: false,
+		},
+	},
+];
 
-const inputs = document.getElementsByTagName('input');
+const ulParent = document.createElement('ul');
 
-const specialLi = document.getElementsByClassName('special');
+for (let game of warriorsGames) {
+	const { homeTeam, awayTeam } = game;
+	// console.log(homeTeam.team, awayTeam.team);
+	const gameLi = document.createElement('li');
+	const { team: hTeam, points: hPoints, isWinner: isHWinner } = homeTeam;
+	const { team: aTeam, points: aPoints, isWinner: isAWinner } = awayTeam;
+	const teamNames = `${aTeam} @ ${hTeam}`;
 
-const h1 = document.querySelector('h1');
+	if (aPoints > hPoints) {
+		const scoreLine = `<b>${aPoints}</b> - ${hPoints}`;
+		gameLi.innerHTML = `${teamNames} ${scoreLine}`;
+	} else {
+		const scoreLine = `${aPoints}- <b>${hPoints}</b>`;
+		gameLi.innerHTML = `${teamNames} ${scoreLine}`;
+	}
+	if (
+		(hTeam === 'Golden State' && isHWinner) ||
+		(aTeam === 'Golden State' && isAWinner)
+	) {
+		gameLi.classList = 'winner';
+	} else {
+		gameLi.classList = 'looser';
+	}
 
-const range = document.querySelector('#range');
-const inner = document.querySelector('#inner');
-
-range.addEventListener('input', function () {
-	inner.style.transform = `rotate(${range.value}deg)`;
-});
+	ulParent.appendChild(gameLi);
+}
+document.body.prepend(ulParent);
